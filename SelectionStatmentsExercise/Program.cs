1c706e2b503937f75c4363fc4aa4ad5a9cbc31a5
Stephen Wills <stephenrwills94@gmail.com>
2022-07-07 11:56:58 -0500

    finished main method

---
+++ SelectionStatmentsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("What is my favorite number?");
+
+var r = new Random();
+var favNumber = r.Next(1, 1000);
+int userInput;
+
+
+
+do
+{
+    Console.WriteLine("Pick a number from 1 to 1000");
+    userInput = int.Parse(Console.ReadLine());
+
+    if (userInput < favNumber)
+    {
+        Console.WriteLine("too low");
+    }
+    else if (userInput > favNumber)
+    {
+        Console.WriteLine("too high");
+    }
+    else
+    {
+        Console.WriteLine("Congrats! You guessed it!");
+    }
+    
+}
+while (userInput != favNumber);
